a52f04c73291c50648d125c467adaaecb0add026
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-08-27 17:24:27 -0600

    feat: :sparkles: Ahora se oculta la barra de lugares cuando hacemos click en "Direcciones"

--- src/app/maps/components/search-results/search-results.component.ts
+++ src/app/maps/components/search-results/search-results.component.ts
@@ -30,8 +30,11 @@ export class SearchResultsComponent {
   getDirections(place: Feature) {
     if (!this.placesService.userLocation) throw Error('No hay userLocation!');
 
+    this.placesService.deletePlaces();
+
     const start = this.placesService.userLocation;
     const end = place.center as [number, number];
+
     this.mapsService.getRouteBetweenPoints(start, end);
   }
 }
